f64683e6592540d69739302aa04a56a7020cd0d9
Dan Hart <dshart@gmail.com>
2026-04-27 08:18:38 -0400

    added HTML generation module called movie-converter.js

---
+++ movieMasterPieces/scripts/movie-converter.js
@@ -0,0 +1,17 @@
+import {movies} from "./movies-data.js";
+
+export const generateMoviesHTML = () => {
+    let moviesHTML = '';
+
+    for (const movie of movies) {
+        movieHTML += `<article class="movie">
+          <img src="${movie.poster}" alt="${movie.title} poster class="movie-poster">
+          <div class="movie-details">
+            <h2>class="movie-title>${movie.title}</h2>
+            <p class = "movie-description">${movie.description}</p>
+          </div> 
+         </article>
+    `;     
+    }
+    return movieHTML
+};
